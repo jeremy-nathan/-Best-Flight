f5a01c4e1ed7589f16e1b291a3cbfee132aaa407
jeremynathan <jeremy.nathan1@gmail.com>
2019-05-22 00:37:58 +0800

    breakthrough

--- bestflight.py
+++ bestflight.py
@@ -1,8 +1,9 @@
 from geopy.geocoders import Nominatim
 import geopy
+from geopy import distance
 import gmplot
 
-cities=['Shanghai','New York','Singapore','New Delhi','Manila','Washington DC','Tokyo','Paris']
+cities=['Kuala Lumpur','Shanghai','New York','Singapore','New Delhi','Manila','Washington DC','Tokyo','Paris']
 cities_latitude=[None] * len(cities)
 cities_longitude=[None] * len(cities)
 
@@ -15,10 +16,13 @@ KL_longitude=KL_location.longitude
 print((KL_latitude,KL_longitude))
 
 
+
+
 for i in range(len(cities)):
     location=geolocator.geocode(cities[i])
     cities_latitude[i]=location.latitude
     cities_longitude[i]=location.longitude
+
 cities_latitude.append(KL_latitude)
 cities_longitude.append(KL_longitude)
 
@@ -26,7 +30,18 @@ print(cities_latitude)
 print(cities_longitude)
 # print(location.raw)
 
+print()
+cities_distance=[None] * len(cities)
+
+for i in range(len(cities)):
+    print("Distance between "+cities[0]+" and "+cities[i])
+    cities_distance[i]=int(distance.distance((cities_latitude[0],cities_longitude[0]),(cities_latitude[i],cities_longitude[i])).kilometers)
+    print(cities_distance[i])
+
+
+
 import gmplot
+
 
 gmap3 = gmplot.GoogleMapPlotter(KL_latitude,
                                 KL_longitude, 13)
@@ -36,5 +51,8 @@ gmap3.scatter( cities_latitude, cities_longitude, '#FF0000',
 
 gmap3.plot(cities_latitude, cities_longitude,
            'red', edge_width = 2.5)
+
+gmap3.apikey="AIzaSyDmpwQtMwmoWGHX2UBqnAldc8CFDus77RQ"
+
 
 gmap3.draw("gmap3.html")
