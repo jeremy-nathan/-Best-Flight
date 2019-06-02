11d50d29cb65552ee18eaaa391dc3498b9f80936
jeremynathan <jeremy.nathan1@gmail.com>
2019-06-02 22:50:41 +0800

    shit

--- bestflight.py
+++ bestflight.py
@@ -2,11 +2,9 @@ from geopy.geocoders import Nominatim
 import geopy
 from geopy import distance
 import gmplot
-import matplotlib.pyplot as plt
-import seaborn as sns
-sns.set()
-import networkx as nx
 import time
+import gmplot
+
 
 start=time.time()
 
@@ -20,14 +18,6 @@ geolocator  = Nominatim(user_agent="Algo Assignment")
 KL_location=geolocator.geocode(origin,timeout = 150)
 KL_latitude=KL_location.latitude
 KL_longitude=KL_location.longitude
-# # from geopy.geocoders import Nominatim
-# #print((KL_latitude,KL_longitude))
-#
-# origin2 = 'Shanghai'
-# Shanghai_location =geolocator.geocode(origin2,timeout = 100)
-# Shanghai_latitude =Shanghai_location.latitude
-# Shanghai_longitude = Shanghai_location.longitude
-
 
 
 geolocator  = Nominatim(user_agent="Algo Assignment")
@@ -36,134 +26,13 @@ for i in range(len(cities)):
     cities_latitude[i]=location.latitude
     cities_longitude[i]=location.longitude
 
-# cities_latitude.append(KL_latitude)
-# cities_longitude.append(KL_longitude)
-
-print(cities_latitude)
-print(cities_longitude)
-# print(location.raw)
-
-print()
-cities_distance=[None] * len(cities)
-
-for i in range(len(cities)):
- for j in range(len(cities)):
-    print("Distance between "+cities[i]+" and "+cities[j])
-    cities_distance[i]=int(distance.distance((cities_latitude[i],cities_longitude[i]),(cities_latitude[j],cities_longitude[j])).kilometers)
-    print(cities_distance[i])
-
-
-# cities_distance_Shanghai ={}
-# for i in range(len(cities)):
-#     cities_distance_Shanghai[cities[i]] = int(distance.distance((Shanghai_latitude,Shanghai_longitude),(cities_latitude[i],cities_longitude[i])).kilometers)
-#
-# print("Shanghai")
-# print(cities_distance_Shanghai)
-#
-# origin3 = 'New York'
-# NewYork_location =geolocator.geocode(origin3,timeout = 100)
-# NewYork_latitude =NewYork_location.latitude
-# NewYork_longitude = NewYork_location.longitude
-#
-#
-#
-# cities_distance_NewYork = {}
-# for i in range(len(cities)):
-#     cities_distance_NewYork[cities[i]] = int(distance.distance((NewYork_latitude,NewYork_longitude),(cities_latitude[i], cities_longitude[i])).kilometers)
-# print('New York')
-# print(cities_distance_NewYork)
-#
-#
-# origin4 = 'Singapore'
-# Singapore_location =geolocator.geocode(origin4)
-# Singapore_latitude =Singapore_location.latitude
-# Singapore_longitude = Singapore_location.longitude
-#
-#
-#
-# cities_distance_Singapore = {}
-# for i in range(len(cities)):
-#     cities_distance_Singapore[cities[i]] = int(distance.distance((Singapore_latitude,Singapore_longitude),(cities_latitude[i], cities_longitude[i])).kilometers)
-# print('Singapore')
-# print(cities_distance_Singapore)
-#
-# origin5 = 'New Delhi'
-# New_Delhi_location = geolocator.geocode(origin5,timeout = 100)
-# New_Delhi_latitude =New_Delhi_location.latitude
-# New_Delhi_longitude = New_Delhi_location.longitude
-#
-#
-#
-# cities_distance_New_Delhi = {}
-#
-# for i in range(len(cities)):
-#      cities_distance_New_Delhi[cities[i]] = int(distance.distance((New_Delhi_latitude,New_Delhi_longitude),(cities_latitude[i], cities_longitude[i])).kilometers)
-# print('New Delhi')
-# print(cities_distance_New_Delhi)
-#
-#
-# origin6 = 'Manila'
-# Manila_location = geolocator.geocode(origin6)
-# Manila_latitude = Manila_location.latitude
-# Manila_longitude = Manila_location.longitude
-#
-# cities_distance_Manila = {}
-#
-# for i in range(len(cities)):
-#      cities_distance_Manila[cities[i]] = int(distance.distance((Manila_latitude,Manila_longitude),(cities_latitude[i], cities_longitude[i])).kilometers)
-# print('Manila')
-# print(cities_distance_Manila)
-#
-# origin7 = 'Washington DC'
-# Washington_DC_location = geolocator.geocode(origin7,timeout=100)
-# Washington_DC_latitude = Washington_DC_location.latitude
-# Washington_DC_longitude = Washington_DC_location.longitude
-#
-#
-# cities_distance_Washington_DC = {}
-#
-# for i in range(len(cities)):
-#      cities_distance_Washington_DC[cities[i]] = int(distance.distance((Washington_DC_latitude,Washington_DC_longitude),(cities_latitude[i], cities_longitude[i])).kilometers)
-# print('Washington DC')
-# print(cities_distance_Washington_DC)
-#
-# origin8 = 'Tokyo'
-# Tokyo_location = geolocator.geocode(origin8)
-# Tokyo_latitude = Tokyo_location.latitude
-# Tokyo_longitude = Tokyo_location.longitude
-#
-#
-# cities_distance_Tokyo = {}
-#
-# for i in range(len(cities)):
-#      cities_distance_Tokyo[cities[i]] = int(distance.distance((Tokyo_latitude,Tokyo_longitude),(cities_latitude[i], cities_longitude[i])).kilometers)
-# print('Tokyo')
-# print(cities_distance_Tokyo)
-#
-#
-# origin9 = 'Paris'
-# Paris_location = geolocator.geocode(origin9, timeout=120)
-# Paris_latitude = Paris_location.latitude
-# Paris_longitude = Paris_location.longitude
-#
-#
-# cities_distance_Paris = {}
-#
-# for i in range(len(cities)):
-#      cities_distance_Paris[cities[i]] = int(distance.distance((Paris_latitude,Paris_longitude),(cities_latitude[i], cities_longitude[i])).kilometers)
-# print('Paris')
-# print(cities_distance_Paris)
-
-import gmplot
-# import numpy as np
-# import pandas as pd
 
 gmap3 = gmplot.GoogleMapPlotter(KL_latitude,
                                 KL_longitude, 13)
 
 gmap3.scatter( cities_latitude, cities_longitude, '#FF0000',
                               size = 100, marker = True )
-#for i in range(len(cities_latitude)):
+
 gmap3.coloricon = "http://www.googlemapsmarkers.com/v1/%s/"
 gmap3.marker(cities_latitude[0],cities_longitude[0],'cornflowerblue')
 
@@ -175,43 +44,13 @@ gmap3.plot(cities_latitude[0:2],cities_longitude[0:2], 'red',edge_width = 2.5)
 for j in range(len(cities_latitude)):
     for i in range(len(cities_latitude)-1):
         gmap3.plot((cities_latitude[j],cities_latitude[i+1]),(cities_longitude[j],cities_longitude[i+1]), 'red',edge_width = 2.5)
-# gmap3.plot(cities_latitude[0:4],cities_longitude[0:4], 'red',edge_width = 2.5)
-
-# def add_line(start):
-#     for i in range(len(cities)):
 
 
 gmap3.apikey="AIzaSyDmpwQtMwmoWGHX2UBqnAldc8CFDus77RQ"
-# for j in range(len(cities)):
-#     for i in range(len(cities)):
+
 for j in range(len(cities)):
      for i in range(len(cities)):
          gmap3.plot(cities_latitude[j:i],cities_longitude[j:i], 'red',edge_width = 2.5)
 
 
 gmap3.draw("gmap.html")
-
-# G = nx.Graph()
-#
-# def add_node(startnode):
-#     for i in range(len(cities)-1):
-#         G.add_edge(cities[startnode],cities[i+1],weight=cities_distance[i+1])
-#
-#
-# for i in range(len(cities)):
-#     add_node(i)
-#
-# pos  = nx.spring_layout(G)
-#
-# nx.draw_networkx_nodes(G,pos,node_size = 700)
-#
-# nx.draw_networkx_edges(G,pos,edgelist=None, width = 6)
-#
-# nx.draw_networkx_labels(G,pos,font_size = 10, font_family = 'sans-serif')
-#
-# plt.axis('off')
-# plt.show()
-#
-# end=time.time()
-#
-# print("Running Time: "+str(end-start))
--- bestflightv2.py
+++ bestflightv2.py
@@ -7,6 +7,9 @@ import random
 start=time.time()
 import webbrowser
 from itertools import combinations
+import string
+import re
+
 
 origin='Kuala Lumpur'
 geolocator  = Nominatim(user_agent="Algo Assignment")
@@ -50,6 +53,162 @@ def calcdistance(start):
 for i in range(len(cities)):
     cities_distance[cities[i]]={}
     calcdistance(i)
+
+
+sent_cities=['Shanghai','New York','Singapore','New Delhi','Washington DC','Tokyo','Paris','Manila']
+
+ratpositivenegative = []
+positiveword = []
+negativeword = []
+
+def calculateposandneg(cities):
+    for x in cities:
+        file1 = open(x+".txt")
+        line = file1.read()# Use this to read file content as a stream:
+        words = line.split()
+        # print(words)
+        allow = string.ascii_letters + string.digits
+        re.sub('[^%s]]' % allow, '', line)
+        table = str.maketrans({key: None for key in string.punctuation})
+        new_s = line.translate(table)
+
+        print("Words with punctuation of "+x+ " articles : " + line)
+        print('\n')
+        print("Words without punctuation "+x+ " articles : "+ new_s)
+        print('\n')
+
+        # tokenized_text=sent_tokenize(text)
+        # print(tokenized_text)
+
+        new_s = new_s.lower()
+        from nltk.tokenize import word_tokenize
+        tokenized_word=word_tokenize(new_s)
+        # print(tokenized_word)
+        print('\n')
+
+
+
+        from nltk.corpus import stopwords
+        stop_words=set(stopwords.words("english"))
+        print(stop_words)
+        print('\n')
+
+        filtered_sent=[]
+        for w in tokenized_word:
+            if w not in stop_words:
+                filtered_sent.append(w)
+
+        stopwords_sent = []
+        for y in tokenized_word:
+            if y in stop_words:
+                stopwords_sent.append(y)
+
+        print("Tokenized Sentence:",tokenized_word)
+        print('\n')
+        print("Filterd Sentence:",filtered_sent)
+        print('\n')
+        print("Stopword Sentence:",stopwords_sent)
+        print('\n')
+        from nltk.probability import FreqDist
+        fdist = FreqDist(filtered_sent)
+        fdist1 = FreqDist(stopwords_sent)
+        print(fdist)
+        print('\n')
+        print("Most Common word :",fdist.most_common(2))
+        print('\n')
+        print("Most Common Stopword :",fdist1.most_common(2))
+
+        # import matplotlib.pyplot as plt
+        # plt.xlabel('Count Words')
+        # plt.ylabel('Frequency')
+        # plt.title('The Frequency of Count Words for : '+x+" articles")
+        # fdist.plot(30,cumulative=False)
+        #
+        # plt.show()
+        #
+        #
+        # import matplotlib.pyplot as plt1
+        # plt1.xlabel('Stop Words')
+        # plt1.ylabel('Frequency')
+        # plt1.title('The Frequency of Stop Words for : '+x+" articles")
+        # fdist1.plot(30,cumulative=False)
+        #
+        # plt1.show()
+
+
+
+
+        appendFile = open('filteredtext1.txt','w')
+        for r in filtered_sent:
+            appendFile.write(" "+r)
+
+        appendFile.close()
+
+        file1 = open("filteredtext1.txt")
+        theTweet = file1.read()  # Use this to read file content as a stream:
+
+        file2 = open("positive.txt")
+        line2 = file2.read()  # Use this to read file content as a stream:
+        positive_words = line2.split(", ")
+
+        file3 = open("negative.txt")
+        line3 = file3.read()  # Use this to read file content as a stream:
+        negative_words = line3.split(", ")
+
+
+
+        theTokens = re.findall(r'\b\w[\w-]*\b', theTweet)
+        print("The Tokenized Words", theTokens)
+        print("\n")
+
+        numPosWords = 0
+        numPos = []
+        for word in theTokens:
+            if word in positive_words:
+                numPosWords += 1
+                numPos.append(word)
+
+        positiveword.append(numPosWords)
+
+        print("Total number of Positive Words: ", numPosWords)
+        print("The Positive words for article "+x+" :", numPos)
+        # numposcities = numPosWords
+        numNegWords = 0
+        numNeg = []
+        for words in theTokens:
+            if words in negative_words:
+                numNegWords += 1
+                numNeg.append(words)
+        # print()
+        # print("Total number of Negative Words: ", numNegWords)
+        # print("The Negative Words for article "+x+" :", numNeg)
+        negativeword.append(numNegWords)
+        numWords = len(theTokens)
+        print(numWords)
+        percntPos = numPosWords / numWords
+        percntNeg = numNegWords / numWords
+        print()
+        print("Positive: " + "{:.0%}".format(percntPos) + "  Negative: " + "{:.0%}".format(percntNeg))
+        print()
+        if numPosWords > numNegWords:
+            print("Positive " + str(numPosWords) + ":" + str(numNegWords))
+            print()
+            print("The Article has a Positive Sentimental Analysis!!!!")
+        elif numNegWords > numPosWords:
+            print("Negative " + str(numPosWords) + ":" + str(numNegWords))
+            print()
+            print("The Article has a Negative Sentimental Analysis!!!!")
+        elif numNegWords == numPosWords:
+            print("Neutral " + str(numPosWords) + ":" + str(numNegWords))
+            print()
+            print("The Article has a Neutral Sentimental Analysis!!!!")
+
+difposneg = []
+calculateposandneg(sent_cities)
+for i in range(len(positiveword)):
+    difposneg.append(positiveword[i]-negativeword[i])
+
+cities_distance['Kuala Lumpur']['Singapore'] -= difposneg[2]
 
 
 def dijkstra(graph,src,dest,visited=[],distances={},predecessors={}):
@@ -109,10 +268,10 @@ def dijkstra(graph,src,dest,visited=[],distances={},predecessors={}):
         x=min(unvisited, key=unvisited.get)
         return dijkstra(graph,x,dest,visited,distances,predecessors)
 
-KL_Manila_Route = {'Kuala Lumpur':{'Singapore':cities_distance['Kuala Lumpur']['Singapore'],'New Delhi':cities_distance['Kuala Lumpur']['New Delhi'],'Paris':cities_distance['Kuala Lumpur']['Paris'], 'Washington DC':cities_distance['Kuala Lumpur']['Washington DC']},
-                    'Singapore':{'Kuala Lumpur':cities_distance['Kuala Lumpur']['Singapore'],'Shanghai':cities_distance['Singapore']['Shanghai'],'Tokyo':cities_distance['Singapore']['Tokyo']},
-                    'New Delhi':{'Kuala Lumpur':cities_distance['Kuala Lumpur']['New Delhi'],'New York':cities_distance['New Delhi']['New York']},
-                    'Paris':{'Kuala Lumpur':cities_distance['Kuala Lumpur']['Paris'],'Shanghai':cities_distance['Shanghai']['Paris']},
+KL_Manila_Route = {'Kuala Lumpur':{'Singapore':(cities_distance['Kuala Lumpur']['Singapore']-difposneg[2]),'New Delhi':cities_distance['Kuala Lumpur']['New Delhi']-difposneg[3],'Paris':cities_distance['Kuala Lumpur']['Paris']-difposneg[6], 'Washington DC':cities_distance['Kuala Lumpur']['Washington DC']-difposneg[4]},
+                    'Singapore':{'Kuala Lumpur':cities_distance['Kuala Lumpur']['Singapore']-difposneg[2],'Shanghai':cities_distance['Singapore']['Shanghai']-(difposneg[2]-difposneg[0]),'Tokyo':cities_distance['Singapore']['Tokyo']-(difposneg[2]-difposneg[5])},
+                    'New Delhi':{'Kuala Lumpur':cities_distance['Kuala Lumpur']['New Delhi']-difposneg[3],'New York':cities_distance['New Delhi']['New York']-(difposneg[3]-difposneg[1])},
+                    'Paris':{'Kuala Lumpur':cities_distance['Kuala Lumpur']['Paris']-difposneg[6],'Shanghai':cities_distance['Shanghai']['Paris']},
                     'Shanghai':{'Singapore':cities_distance['Singapore']['Shanghai'],'Manila':cities_distance['Manila']['Shanghai'],'Paris':cities_distance['Shanghai']['Paris'],'Washington DC':cities_distance['Shanghai']['Washington DC']},
                     'Tokyo':{'Singapore':cities_distance['Singapore']['Tokyo'],'Manila':cities_distance['Manila']['Tokyo']},
                     'Washington DC':{'Kuala Lumpur':cities_distance['Kuala Lumpur']['Washington DC'],'Shanghai':cities_distance['Shanghai']['Washington DC'],"New York":cities_distance["New York"]["Washington DC"]},
@@ -420,6 +579,9 @@ for x in name:
     plt.title('The Total of Positive and Negative Sentiment of Article')
     plt.ylabel("Total Words")
     plt.show()
+
+    winsound.PlaySound("Sound", winsound.SND_FILENAME)
+
 def AnalaysisArticle(positive, negative, neutral, fullword, dic):
     freq = getFreq(positive, negative, neutral, fullword, dic)
     percent = getPercent(freq["Positive"], freq["Negative"], freq["Neutral"], freq["Full"])
@@ -428,7 +590,7 @@ def AnalaysisArticle(positive, negative, neutral, fullword, dic):
 
 
 
-    
+
 def AnalysisFreq(percent):
     print ("Article is", max(percent, key=lambda i: percent[i]))
 
@@ -492,7 +654,7 @@ def printAnalysis(results):
 
 
 
-    winsound.PlaySound("Sound", winsound.SND_FILENAME)
+    # winsound.PlaySound("Sound", winsound.SND_FILENAME)
 
 
     from gtts import gTTS
